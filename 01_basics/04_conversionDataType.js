61db2c3e40eda06287e2f6b3dbe9fe1330ef9064
MangeshCoder <159568679+MangeshCoder@users.noreply.github.com>
2024-07-12 21:11:59 +0000

    comparision operator

--- 01_basics/04_conversionDataType.js
+++ 01_basics/04_conversionDataType.js
@@ -1,10 +1,10 @@
 // convert string or another data type in Number
 
-let  name = undefined;
 // let name = "33abc"  its getting error after convert in number -NaN
 // let name = true  its getting boolean value if true -1 / false -2
 // let name = null getting null - 0 value 
 // let name = undefined getting NaN value
+let  name = undefined;
 console.log("Type  of name before convert is ",typeof name);
 console.log("value of datatype",name)
 
@@ -12,13 +12,15 @@ let convertName = Number(name) // for convert dataType to number we use Number b
 console.log("Type of name after convert is ",typeof convertName);
 console.log(convertName);
 
+//===============================*************==================================
+
 //convert dataType into boolean
 
-let id = "hello";
 // let id ="name" convert to booleant value true
 // let id =" " convert to boolean value false
 // let id = null convert to boolean value false
 // let id = undefined  convert to boolean value false 
+let id = "hello";
 console.log("Type of id before convert is ", typeof id);
 console.log(id);
 
---
+++ 01_basics/05_comparisionOperator.js
@@ -0,0 +1,36 @@
+// Comparision operators
+
+/*
+ Arithmatic operator -- + ,- ,* ,/ ,% ,**
+let a = 10;
+let b = 20;
+console.log("Value of a + b is ",a+b);
+console.log("Value of a - b is ",a-b);
+console.log("Value of a * b is ",a*b);
+console.log("Value of a / b is ",a/b)
+console.log("value of a % b is ",a%b);
+console.log("Value of a ** b is",a**b);
+console.log("Value of a++ is",a++);
+console.log("Value of a-- is",a--);
+
+*/
+
+//Comparision operatores
+
+let a = 10;
+let b = 20;
+
+console.log("a is greater than b -- ",a>b)
+console.log("a is less than b -- ",a<b);
+console.log("a is greater than and equal to b -- ",a>=b);
+console.log("a is less than and equal to b -- ",a<=b);
+console.log("a is == b or not",a==b);
+console.log("a is != b or not",a!=b);
+console.log("a is strict equal to b --",a===b);
+console.log("a is strict not equal to b --",a!==b);
+
+
+
+
+
+
